56fb58e353ced8d29b26e0bdc9dcbc135d790b0c
rashid-shahriar <rashid-shahriar>
2025-05-04 16:07:26 +0600

    added role based auth system

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,8 +6,11 @@
 </head>
 
 <body>
+    {{ $user = Auth::user()->load('role') }}
     <h2>Welcome, {{ auth()->user()->name }}</h2>
     <p>Your email: {{ auth()->user()->email }}</p>
+    <p>Your ID: {{ auth()->user()->id }}</p>
+    <p>Your are admin: {{ Auth::check() && (Auth::user()->role->name == 'admin') == 'admin' ? 'True' : 'False' }}</p>
     <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit">Logout</button>
